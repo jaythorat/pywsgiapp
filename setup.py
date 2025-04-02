f5a9ba6fa490465a910b544a6cbf0ad27667b19d
Jay Thorat <jaythorat.dev@gmail.com>
2025-04-02 14:25:15 +0530

    Updated minor version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pywsgiapp",
-    version="0.0.1",
+    version="0.0.2",
     description="A lightweight WSGI application",
     long_description=long_description, 
     long_description_content_type="text/markdown",  
